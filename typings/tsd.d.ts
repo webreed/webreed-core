a4d0361a5f5b741a23af5263d453af44bcf1eb24
Lea Hayes <lea@rotorz.com>
2016-03-07 18:02:20 +0000

    Add type definitions for unit mocha and should.js.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,5 @@
 /// <reference path="glob/glob.d.ts" />
 /// <reference path="minimatch/minimatch.d.ts" />
 /// <reference path="rimraf/rimraf.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="should/should.d.ts" />
