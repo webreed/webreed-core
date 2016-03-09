783a4955a1bc5a3b1ed03d57c3c54b37b931020b
Lea Hayes <lea@rotorz.com>
2016-03-09 01:37:20 +0000

    Remove TypeScript declarations that are currently not used.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,6 +2,4 @@
 /// <reference path="glob/glob.d.ts" />
 /// <reference path="minimatch/minimatch.d.ts" />
 /// <reference path="rimraf/rimraf.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="should/should.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
