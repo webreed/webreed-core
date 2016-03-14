81c921e8090623b39c6700467d9976970596b60b
Lea Hayes <lea@rotorz.com>
2016-03-14 09:37:41 +0000

    Ensure that output path exists before saving an output resource.

--- src/behaviors/processSourceContentFiles.ts
+++ src/behaviors/processSourceContentFiles.ts
@@ -2,6 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root.
 
 
+import * as path from "path";
+
+const fs = require("fs-promise");
 import {Observable} from "rxjs";
 
 import {Environment} from "../Environment";
@@ -27,14 +30,16 @@ async function processSourceContent(env: Environment, contentRelativePath: strin
     let outputRelativePath = env.getOutputRelativePathForResource(
         outputResource._path, outputResource._extension, outputResource._page
       );
-    let outputFilePath = env.resolvePath("output", outputRelativePath);
-
-    return promise.then(
-      () => env.behaviors.saveResourceFile(outputFilePath, outputResource)
-    );
+    return saveOutputResource(env, outputRelativePath, outputResource);
   };
 
   return generatedResourceStream
     .reduce(reducer, Promise.resolve())
     .toPromise();
+}
+
+async function saveOutputResource(env: Environment, outputRelativePath: string, resource: Resource): Promise<void> {
+  let outputFilePath = env.resolvePath("output", outputRelativePath);
+  await fs.mkdirp(path.dirname(outputFilePath));
+  await env.behaviors.saveResourceFile(outputFilePath, resource);
 }
