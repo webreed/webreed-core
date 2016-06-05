fdf4b0cca5d08396b4d9eaeaf1cb1f10b9dc7ae7
Lea Hayes <lea@rotorz.com>
2016-06-05 06:52:54 +0100

    Introduce facade for pagination modules.

---
+++ src/pagination/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Rotorz Limited. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root.
+
+
+export {PaginationIterator} from "./PaginationIterator";
+export {paginate} from "./paginate";
+
